0d36ade9ff165cd0ed701ef4b18abfc8cdcf80be
Vinas-K-Lathiya <118763065+Vinas-K-Lathiya@users.noreply.github.com>
2023-03-20 10:34:57 +0530

    task1

---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "3000-project-1",
+    debugShowCheckedModeBanner: false,
+    home: SafeArea( child: Text("                 Hello\n\n\n\n\n                 Dart\n\n\n\n\n                 flutter"),),
+  ));
+}
