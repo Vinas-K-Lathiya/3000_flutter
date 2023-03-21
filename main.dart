136fd44eef0a764c03ac4b52d117a43f727e8c3c
Vinas-K-Lathiya <118763065+Vinas-K-Lathiya@users.noreply.github.com>
2023-03-21 10:28:59 +0530

    flutter_task_2

--- main.dart
+++ main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 void main(){
   runApp(MaterialApp(
-    title: "3000-project-1",
     debugShowCheckedModeBanner: false,
-    home: SafeArea( child: Text("                 Hello\n\n\n\n\n                 Dart\n\n\n\n\n                 flutter"),),
+    title: "hiii",
+    home: Scaffold(
+      backgroundColor: Color(0xFFfafafa),
+      appBar: AppBar(
+        backgroundColor: Color(0xffff5252),
+        centerTitle: true,
+        title: Text("Flutter App",style: TextStyle(color: Color(0xFFffe5e5)),),
+        leading: Icon(Icons.menu),
+      ),
+      body: Center(child: Text('''            Red & white group of institutes 
+     one step in changing education chain ...''',style: TextStyle(color: Color(0xFFfe5858),fontSize: 20),),),
+    ),
   ));
 }
