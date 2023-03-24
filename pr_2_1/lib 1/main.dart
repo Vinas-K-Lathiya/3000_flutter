411c62f423ff92651d6a056304962e04f1d36a16
Vinas-K-Lathiya <118763065+Vinas-K-Lathiya@users.noreply.github.com>
2023-03-24 10:05:40 +0530

    Add files via upload

---
+++ pr_2_1/lib 1/main.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "jdsfyhrjdn gkrds8 ",
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Color(0xffff5252),
+          centerTitle: true,
+          title: Text("Red & White"),
+        ),
+        body: SafeArea(
+          child: Center(
+            child: RichText(
+              text: TextSpan(
+                  text: "                G",
+                  style: TextStyle(color: Colors.green, fontSize: 18),
+                  children: [
+                    TextSpan(
+                      text: " R ",
+                      style: TextStyle(color: Colors.red, fontSize: 25),
+                    ),
+                    TextSpan(
+                      text: "A P H I C S\n\n",
+                      style: TextStyle(color: Colors.green, fontSize: 18),
+                    ),
+                    TextSpan(
+                      text: "    F L U T T ",
+                      style: TextStyle(color: Colors.blue, fontSize: 18),
+                    ),
+                    TextSpan(
+                      text: "E ",
+                      style: TextStyle(color: Colors.red, fontSize: 25),
+                    ),
+                    TextSpan(
+                      text: "R\n\n",
+                      style: TextStyle(color: Colors.blue, fontSize: 18),
+                    ),
+                    TextSpan(
+                      text: "             A N ",
+                      style: TextStyle(color: Colors.green, fontSize: 18),
+                    ),
+                    TextSpan(
+                      text: "D ",
+                      style: TextStyle(color: Colors.red, fontSize: 25),
+                    ),
+                    TextSpan(
+                      text: "R O I D \n\n",
+                      style: TextStyle(color: Colors.green, fontSize: 18),
+                    ),
+                    TextSpan(
+                      text: "D E S I G N ",
+                      style: TextStyle(color: Colors.yellow, fontSize: 18),
+                    ),
+                    TextSpan(
+                      text: "& ",
+                      style: TextStyle(color: Colors.red, fontSize: 25),
+                    ),
+                    TextSpan(
+                      text: "D E V L O P\n\n",
+                      style: TextStyle(color: Colors.yellow, fontSize: 18),
+                    ),
+                    TextSpan(
+                      text: "              W ",
+                      style: TextStyle(color: Colors.red, fontSize: 25),
+                    ),
+                    TextSpan(
+                      text: "E B \n\n",
+                      style: TextStyle(color: Colors.blue, fontSize: 18),
+                    ),
+                    TextSpan(
+                      text: "          F A S ",
+                      style: TextStyle(color: Colors.yellow, fontSize: 18),
+                    ),
+                    TextSpan(
+                      text: "H ",
+                      style: TextStyle(color: Colors.red, fontSize: 25),
+                    ),
+                    TextSpan(
+                      text: "I O N \n\n",
+                      style: TextStyle(color: Colors.yellow, fontSize: 18),
+                    ),
+                    TextSpan(
+                      text: "A N I M A T ",
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 40, 117, 43),
+                          fontSize: 18),
+                    ),
+                    TextSpan(
+                      text: "I ",
+                      style: TextStyle(color: Colors.red, fontSize: 25),
+                    ),
+                    TextSpan(
+                      text: "O N \n\n",
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 40, 117, 43),
+                          fontSize: 18),
+                    ),
+                    TextSpan(
+                      text: "                  I ",
+                      style: TextStyle(color: Colors.blue, fontSize: 18),
+                    ),
+                    TextSpan(
+                      text: "T ",
+                      style: TextStyle(color: Colors.red, fontSize: 25),
+                    ),
+                    TextSpan(
+                      text: "A - C S +\n\n",
+                      style: TextStyle(color: Colors.blue, fontSize: 18),
+                    ),
+                    TextSpan(
+                      text: "        G A M ",
+                      style: TextStyle(color: Colors.orange, fontSize: 18),
+                    ),
+                    TextSpan(
+                      text: "E ",
+                      style: TextStyle(color: Colors.red, fontSize: 25),
+                    ),
+                  ]),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
